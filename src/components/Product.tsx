54851fe0968003ef0abea98e5bd40601029ecd25
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-07-18 18:57:10 +0300

    поставил пакет propTypes

---
+++ src/components/Product.tsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+interface ProductProps {
+  name: string;
+  imgUrl: string;
+  price: number;
+}
+
+export default function Product({ name, imgUrl, price }: ProductProps) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="320" />
+      <p>Price: {price} credits</p>
+    </div>
+  );
+}
+
+Product.propTypes = {
+  name: PropTypes.string,
+  imgUrl: PropTypes.string,
+  price: PropTypes.number,
+};
